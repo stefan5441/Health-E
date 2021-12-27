aae3f4ffa29580dba2a1e037276d133a36601f95
stefan5441 <stefan.cambov2000@gmail.com>
2021-12-27 15:32:44 +0100

    setting black navbar

--- Domashna 3/healthe_app/resources/js/mapbox.js
+++
@@ -1,6 +0,0 @@
-mapboxgl.accessToken = 'pk.eyJ1Ijoic3RlZmFuZGlhbnMiLCJhIjoiY2t4aHAwZWg2MnVjeTMwbzFtam54enpqYiJ9.B264t4XyHpD9f0t-sSooMQ';
-
-var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11'
-});
--- Domashna 3/healthe_app/resources/views/results.blade.php
+++ Domashna 3/healthe_app/resources/views/results.blade.php
@@ -7,12 +7,12 @@
         <title>Health-E</title>
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <!-- JQuery  -->
+        <script src="https://code.jquery.com/jquery-1.9.1.min.js"></script>  
         <!-- Bootstrap -->
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         <!-- CSS -->
-        <link href="{{ asset('css/results.css') }}" rel="stylesheet">
-
-        
+        <link href="{{ asset('css/results.css') }}" rel="stylesheet">      
 
         <script src='https://api.mapbox.com/mapbox-gl-js/v2.3.1/mapbox-gl.js'></script>
         <link href='https://api.mapbox.com/mapbox-gl-js/v2.3.1/mapbox-gl.css' rel='stylesheet' />
@@ -23,7 +23,7 @@
                 direction: rtl;
             }
             .table {
-                height: 100%;
+                height: 30rem;
             }
             #tableMap{
                 margin-top: 4rem;
@@ -31,9 +31,68 @@
             #searchNav {
                 margin-top: 4rem;
             }
+            .table thead tr {
+                line-height: 2rem
+            }
+            .table thead tr th {
+                text-align: center;
+                vertical-align: middle;
+            }
+            .table tbody tr {
+                line-height: 2rem
+            }
+            .table tbody tr th {
+                text-align: center;
+                vertical-align: middle;
+            }
+            .table tbody tr td {
+                text-align: center;
+                vertical-align: middle;
+            }
+            body {
+                /* background-image: url({{url('images/backgroundImage.png')}}); */
+                background-size: cover;
+            }
+            #blackNav ul li{
+                margin-left: 1rem;
+                margin-right: 1rem;
+            }
+            #blackNav ul {
+                margin-left: 1rem;
+                margin-right: 1rem;
+            }
+            
+            #blackNav {
+                height: 2.5rem;
+            }
         </style>
     </head>
     <body class="antialiased">
+
+        {{-- Helper Navbar  --}}
+        <nav class="navbar navbar-expand-sm navbar-dark bg-dark" id="blackNav">
+            <div class="collapse navbar-collapse justify-content-between">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Почетна</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">За нас</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Помош</a>
+                    </li>
+                </ul>
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Македонски</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Англиски</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
 
         {{-- NavBar --}}
         <div class="container" id="searchNav">
@@ -51,8 +110,8 @@
                 </div>
 
                 <div class="col-sm-4 pl-sm-3" id="navbarButtons">
-                    <button type="button" class="btn btn-danger">Освежи локација</button>
-                    <button type="button" class="btn btn-secondary">Додади твоја локација</button>
+                    <button type="button" class="btn btn-danger" onclick="myLocation()">Освежи локација</button>
+                    <button type="button" class="btn btn-secondary" onclick="myLocation()">Додади твоја локација</button>
                 </div>
             </div>
         </div>
@@ -81,9 +140,14 @@
                                     <td>www.google.com</td>
                                 </tr>
                                 <?php 
-                                    echo "<script type='text/javascript'>addMarker('$user->long', '$user->lat');</script>"
+                                    echo "<script type='text/javascript'>
+                                        $(document).ready(function() {
+                                            addMarker('$user->long', '$user->lat');
+                                        });
+                                        </script>"
                                 ?>
                             @endforeach
+
                         </tbody>
                     </table>
                     <span>
@@ -91,7 +155,7 @@
                     </span>
                 </div>
                 <div class="col-sm-6">
-                    <div id='map' style='width: 100%; height: 100%;'></div>
+                    <div id='map' style='width: 100%; height: 30rem;'></div>
                 </div>
             </div>
         </div>
@@ -104,12 +168,28 @@
                 container: 'map',
                 style: 'mapbox://styles/mapbox/streets-v11',
                 center: [21.4254, 41.9981],
-                zoom: 10
+                zoom: 12
             });
+
             function addMarker(lng, lat){
                 const marker = new mapboxgl.Marker()
                     .setLngLat([lng, lat])
                     .addTo(map);
+            }
+
+            function myLocation(){
+                console.log("YEP");
+                navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {enableHighAccuracy: true });
+            }
+
+            function successLocation(position){
+                const myMarker = new mapboxgl.Marker({
+                    color: "#dd0000"
+                }).setLngLat([position.coords.longitude, position.coords.latitude]).addTo(map);
+            }
+
+            function errorLocation(){
+                alert("You must enable location for this option.")
             }
         </script>
         <!-- Bootstrap -->
